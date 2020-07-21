68176467fee21645cea21da80e1541c1a52865ab
bubblegumi <eliamoftd@gmail.com>
2020-07-21 15:10:50 +0300

    second lesson

--- js/script.js
+++ js/script.js
@@ -8,11 +8,25 @@ const personalMovieDB = {
     private: false
 };
 
-const a = prompt('Last movie you watched?', ''),
-    b = prompt('Rate a movie?', ''),
-    c = prompt('Last movie you watched?', ''),
-    d = prompt('Rate a movie?', '');
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Last movie you watched?', ''),
+        b = prompt('Rate a movie?', '');
+    if (a != null && b !=null && a != '' && b != ''&& a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('Done');
+    } else {
+        console.log('Error');
+        i--;
+    }
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+}
+if (personalMovieDB.count < 10) {
+    console.log('Noob');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log('Great job');
+} else if (personalMovieDB.count >= 30) {
+    console.log('Pro');
+} else {
+    console.log('Error');
+}
 console.log(personalMovieDB);
