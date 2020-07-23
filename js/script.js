1835c088e4134e37cc3afadc2c5a7d1ad652de10
bubblegumi <eliamoftd@gmail.com>
2020-07-23 18:07:51 +0300

    web-site

--- js/script.js
+++ js/script.js
@@ -1,70 +1,34 @@
-const personalMovieDB = {
-    count: 0,
-    movies: {},
-    actors: {},
-    geners: [],
-    private: false,
-    start: function() {
-        personalMovieDB.count = +prompt('How many movies you have watched?', '');
-    
-    while (personalMovieDB.count == '' || personalMovieDB.count == null || isNaN(personalMovieDB.count)) {
-            personalMovieDB.count = +prompt ('How many movies you have watched?', '');
-        }
-    },
-    rememberMyFilms: function() {
-        for (let i = 0; i < 2; i++) {
-            const a = prompt('Last movie you watched?', ''),
-                b = prompt('Rate a movie?', '');
-            if (a != null && b !=null && a != '' && b != ''&& a.length < 50) {
-                personalMovieDB.movies[a] = b;
-                console.log('Done');
-            } else {
-                console.log('Error');
-                i--;
-            }
-        
-        }
-    },
-    personalLevel: function() {
-        if (personalMovieDB.count < 10) {
-            console.log('Noob');
-        } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-            console.log('Great job');
-        } else if (personalMovieDB.count >= 30) {
-            console.log('Pro');
-        } else {
-            console.log('Error');
-        }
-    },
-    showMyDB: function(hidden) {
-        if (!hidden){
-            console.log(personalMovieDB);
-        }
-    },
-    toggleVisibleMyDB: function () {
-        if (personalMovieDB.private) {
-            personalMovieDB.private = false;
-        } else {
-            personalMovieDB.private = true;
-        }
-    },
-    writeYourGenres: function() {
-        for (let i = 1; i <= 3; i++) {
-            let genre = prompt(`Your favourite genre ${i}`);
-            if (genre === '' || genre === null) {
-                console.log('not correct answer');
-                i--;
-            } else {
-                personalMovieDB.geners[i - 1] = genre;
-            }
-        }
-        personalMovieDB.geners.forEach ((item, i) => {
-            console.log(`Favourite genre ${i + 1} - is ${item}`);
-        });
-    }
+const adv = document.querySelectorAll('.promo__adv img'),
+    poster = document.querySelector('.promo__bg'),
+    genre = poster.querySelector('.promo__genre'),
+    movieList = document.querySelector('.promo__interactive-list');
+
+const movieDB = {
+    movies: [
+        'Logan',
+        'League of justice',
+        'Addiction',
+        'La la land',
+        'Scott Loxter',
+    ]
 };
 
+adv.forEach(item => {
+    item.remove();
+});
 
+genre.textContent = 'Drama';
 
+poster.style.backgroundImage = 'url("img/bg.jpg")';
 
+movieList.innerHTML ="";
 
+movieDB.movies.sort();
+
+movieDB.movies.forEach((film, i) => {
+    movieList.innerHTML += `
+    <li class="promo__interactive-item">${i + 1} ${film};
+        <div class="delete"></div>
+    </li>
+    `;
+});
